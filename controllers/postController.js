8965ca78e787c0255926b4c3c936f44cd2e1a58f
Luan Vitor <luan_vittor@hotmail.com>
2022-03-11 00:12:59 -0300

    almost done 8

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,5 +1,6 @@
 const { BlogPosts } = require('../models');
 const { Users } = require('../models');
+// const { Categories } = require('../models');
 
 const post = async (req, res) => {
   const { data } = req.user;
@@ -10,6 +11,20 @@ const post = async (req, res) => {
   return res.status(201).json(newPost);
 };
 
+const getPostById = async (req, res) => {
+  const { id } = req.params;
+  const result = await BlogPosts.findAll({
+    where: { id },
+    attributes: { exclude: ['UserId'] },
+    include: [
+      { model: Users, as: 'user', attributes: { exclude: ['password', 'UserId'] } },
+      // { model: Categories, as: 'categories', through: { attributes: [] } },
+    ],
+  });
+  return res.status(200).json(result);
+};
+
 module.exports = {
   post,
+  getPostById,
 };
