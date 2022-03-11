b9867f9123592b2a467dc8551a8cdaaa0242cee7
Luan Vitor <luan_vittor@hotmail.com>
2022-03-10 21:51:41 -0300

    validation seven done

--- middlewares/validatePost.js
+++ middlewares/validatePost.js
@@ -1,15 +1,27 @@
+const { Categories } = require('../models');
+
 const validateTitle = (req, res, next) => {
+  const { title } = req.body;
+  if (!title) return res.status(400).json({ message: '"title" is required' });
+  next();
+};
 
-}
+const validateContent = (req, res, next) => {
+  const { content } = req.body;
+  if (!content) return res.status(400).json({ message: '"content" is required' });
+    next();
+};
 
-// const = (req, res, next) => {
-  
-// }
+const validateCategoryIds = (req, res, next) => {
+  const { categoryIds } = req.body;
+  if (!categoryIds) return res.status(400).json({ message: '"categoryIds" is required' });
+  const exist = Categories.findByPk(categoryIds);
+  if (!exist) res.status(400).json({ message: '"categoryIds" not found' });
+  return next();
+};
 
-// const = (req, res, next) => {
-  
-// }
-
-// const = (req, res, next) => {
-  
-// }
+module.exports = {
+  validateCategoryIds,
+  validateContent,
+  validateTitle,
+};
