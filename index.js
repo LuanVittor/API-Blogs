1b7cc9276ddfd321a9ef0c3071528f659aef49cc
Luan Vitor <luan_vittor@hotmail.com>
2022-04-07 21:58:26 -0300

    subindo para meu github

--- index.js
+++ index.js
@@ -30,7 +30,6 @@ app.delete('/user/me', auth, deleteMe);
 
 app.listen(3000, () => console.log('ouvindo porta 3000!'));
 
-// não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (request, response) => {
   response.send();
 });
